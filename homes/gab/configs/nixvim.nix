892422f475ea0c72dead5de4c6c0bba5b1a52a2a
git-morin <dev.gab.morin@gmail.com>
2025-02-06 12:56:40 -0500

    :wrench: nixvim init config

--- homes/gab/configs/nixvim.nix
+++ homes/gab/configs/nixvim.nix
@@ -1,11 +1,12 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+let
+  lib = import ../../../lib;
+  wrapForUseAsNixvimConfig = 
+    config:
+      { programs.nixvim = config; };
+in
+{
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
-  ];
-  
-  programs.nixvim = {
-    enable = true;
-    colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
-  };
+  ] ++ map wrapForUseAsNixvimConfig (lib.importNixFiles ./nixvim);
 }
