60603e42bfcdd678e0bd88e9007f71d290fe0bf1
git-morin <dev.gab.morin@gmail.com>
2025-03-20 20:11:59 -0400

    :wrench: Fix bluetooth

--- hosts/main/hardware.nix
+++ hosts/main/hardware.nix
@@ -3,4 +3,9 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.enableRedistributableFirmware = lib.mkDefault true;
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
 }
