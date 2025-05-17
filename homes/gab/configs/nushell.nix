eed0dc8fe5448125a0cac0e8e26f44cddd4bccbb
git-morin <dev.gab.morin@gmail.com>
2025-05-17 11:53:46 -0400

    :shell: Tweaked nushell config

--- homes/gab/configs/nushell.nix
+++ homes/gab/configs/nushell.nix
@@ -2,32 +2,6 @@
     programs.nushell = {
         enable = true;
         configFile.source = ./nushell/config.nu;
-        extraConfig = ''
-           let carapace_completer = {|spans|
-           carapace $spans.0 nushell ...$spans | from json
-           }
-           $env.config = {
-            show_banner: false,
-            completions: {
-            case_sensitive: false # case-sensitive completions
-            quick: true    # set to false to prevent auto-selecting completions
-            partial: true    # set to false to prevent partial filling of the prompt
-            algorithm: "fuzzy"    # prefix or fuzzy
-            external: {
-            # set to false to prevent nushell looking into $env.PATH to find more suggestions
-                enable: true
-            # set to lower can improve completion performance at the cost of omitting some options
-                max_results: 100
-                completer: $carapace_completer # check 'carapace_completer'
-              }
-            }
-           }
-           $env.PATH = ($env.PATH |
-           split row (char esep) |
-           prepend /home/gab/.apps |
-           append /usr/bin/env
-           )
-        '';
         shellAliases = {
             switch   = "nh os switch #main";  # evaluate -> build -> switch to new generation
             test     = "nh os test #main";  # evaluate -> test
---
+++ homes/gab/configs/nushell/config.nu
@@ -0,0 +1,36 @@
+let carapace_completer = { |spans| carapace $spans.0 nushell ...$spans | from json }
+$env.config = {
+  if $nu.is-interactive {
+    show_banner
+  } else {
+    show_banner: false
+  }
+  completions: {
+    case_sensitive: false
+    quick: true
+    partial: true
+    algorithm: "fuzzy"
+    external: {
+      enable: true
+      max_results: 100
+      completer: $carapace_completer
+    }
+  }
+}
+$env.PATH = ($env.PATH | split row (char esep) | prepend /home/gab/.apps | append /usr/bin/env )
+
+
+def show_banner [] {
+    let ellie = [
+        "     __  ,"
+        " .--()°'.'"
+        "'|, . ,'  "
+        ' !_-(_\   '
+    ]
+    let s_mem = (sys mem)
+    let s_ho = (sys host)
+    print $"(ansi reset)(ansi green)($ellie.0)"
+    print $"(ansi green)($ellie.1)  (ansi yellow) (ansi yellow_bold)Nushell (ansi reset)(ansi yellow)v(version | get version)(ansi reset)"
+    print $"(ansi green)($ellie.2)  (ansi light_blue) (ansi light_blue_bold)RAM (ansi reset)(ansi light_blue)($s_mem.used) / ($s_mem.total)(ansi reset)"
+    print $"(ansi green)($ellie.3)  (ansi light_purple)ﮫ (ansi light_purple_bold)Uptime (ansi reset)(ansi light_purple)($s_ho.uptime)(ansi reset)"
+}
