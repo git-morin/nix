419bb961c660742aec0b5fcd3d5f9c0f87c25ff4
git-morin <dev.gab.morin@gmail.com>
2025-05-18 23:41:03 -0400

    :sparkles: Some more tweaking, nothing specific here

--- hosts/common/programs.nix
+++ hosts/common/programs.nix
@@ -1,3 +1,11 @@
 {
-  programs.firefox.enable = true;
+  programs = {
+    firefox.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+  };
 }
